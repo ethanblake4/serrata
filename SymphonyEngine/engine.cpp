88ceaa3782c86220261b5199b766f7d0877a146c
ethanblake4 <ethanblake@gmail.com>
2018-10-14 22:35:00 -0700

    Engine initial commit

---
+++ SymphonyEngine/engine.cpp
@@ -0,0 +1,12 @@
+// The main engine with all of the exported methods
+extern "C" {
+
+	__declspec(dllexport) int initializeEngine() {
+		
+	}
+
+	__declspec(dllexport) int getAudioDevices() {
+
+	}
+
+}
